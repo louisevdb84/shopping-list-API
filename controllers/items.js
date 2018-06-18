7b58903c67be0a4f7446fa7183eaed25f5ef4599
Louise van den Berg <louisevdb84@gmail.com>
2018-06-18 16:21:45 +0200

    Add items functions

--- controllers/items.js
+++ controllers/items.js
@@ -12,28 +12,69 @@ const getItems = (req, res) => {
     })
 }
 
-const addEditItems = () => {
-    Shop.findOne({ name: 'Food Lovers Market' }, (err, shop) => {
-        Status.findOne({ name: 'Permanent' }, (err, status) => {
-            Item.create({
-                name: "Rice",
-                shop: shop,
-                sorting: 90,
-                isRepeating: true,
-                status: status
-            }, (err, item) => {
-                if (err) {
-                    console.log(err);
-                 } else {
-                     console.log(item);
-                 }
-            })
-        })
-    });    
+const getItemsByStatus = (req, res) => { 
+    const { status } = req.body;    
+    Item.find({'status.name' : status}, (err, item) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(item);            
+        }
+    })
 }
+
+const newItem = (req, res) => {
+    console.log("SOMETHING")
+    const { name, shop, sorting, isRepeating, status } = req.body;
+    console.log(req.body);
+    if (!name) {
+        return res.status(400).json('incorrect form submission');
+    }
+    Item.create({
+        name: name,
+        shop: {
+            id: shop.id,
+            name: shop.name
+        },
+        sorting: sorting,
+        isRepeating: isRepeating,
+        status: {
+            id: status.id,
+            name: status.name
+        }
+    }, (err, item) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(item);
+        }
+    });
+}
+
+// const addEditItems = () => {
+//     Shop.findOne({ name: 'Food Lovers Market' }, (err, shop) => {
+//         Status.findOne({ name: 'Permanent' }, (err, status) => {
+//             Item.create({
+//                 name: "Rice",
+//                 shop: shop,
+//                 sorting: 90,
+//                 isRepeating: true,
+//                 status: status
+//             }, (err, item) => {
+//                 if (err) {
+//                     console.log(err);
+//                  } else {
+//                      console.log(item);
+//                  }
+//             })
+//         })
+//     });    
+// }
 
 module.exports = {
     getItems,
-    addEditItems
+    //addEditItems,
+    getItemsByStatus,
+    newItem
 }
 
--- server.js
+++ server.js
@@ -14,10 +14,14 @@ app.use(bodyParser.json());
 mongoose.connect("mongodb://louise:shoppinglist000@ds119049.mlab.com:19049/shoppinglist");
 
 app.get('/items', (req, res) => { items.getItems(req, res) })
+app.post('/items', (req, res) => { items.getItemsByStatus(req, res) })
+app.post('/items/new', (req, res) => { items.newItem(req, res) })
+
 app.get('/shops', (req, res) => { shops.getShops(req, res) })
 app.post('/shops/new', (req, res) => { shops.newShop(req, res) })
 app.put('/shops/:id/edit', (req, res) => { shops.updateShop(req, res) })
 app.delete('/shops/delete', (req, res) => { shops.deleteShop(req, res) })
+
 app.get('/status', (req, res) => { status.getStatus(req, res) })
 
 
